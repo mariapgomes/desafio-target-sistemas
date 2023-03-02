13da1665512eeda38385ec972c4082aa0df09909
Maria Gomes <mariapg.dev@gmail.com>
2023-03-02 20:00:03 -0300

    Questão 1 resolvida

---
+++ script.js
@@ -0,0 +1,18 @@
+// Observe o trecho de código abaixo:
+
+// int INDICE = 13, SOMA = 0, K = 0;
+
+// enquanto K < INDICE faça
+
+// {
+
+// K = K + 1;
+
+// SOMA = SOMA + K;
+
+// }
+
+// imprimir(SOMA);
+
+// Ao final do processamento, qual será o valor da variável SOMA?
+// Resposta: A variável SOMA terá o mesmo valor da variável K porque as duas iniciam com valor zero, então nesse caso o valor da variável SOMA será 1
